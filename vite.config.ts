fcb8358b84ac0902b031dcfde68e79a889df10cf
kenneac <iamkenneac@gmail.com>
2026-06-08 08:02:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/A-Bit-of-Everything-in-JavaScript/',
+  base:   process.env.VITE_DEPLOY_TARGET === 'github'
+      ? '/A-Bit-of-Everything-in-JavaScript/'
+      : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
